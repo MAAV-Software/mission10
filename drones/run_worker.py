3ce956103167646256f9c80de40c0e144b8fcb0b
DervinTian <dervint@umich.edu>
2026-08-06 18:36:15 +0000

    Updated the run scripts

--- drones/run_worker.py
+++ drones/run_worker.py
@@ -7,7 +7,7 @@ def main():
     hostname = socket.gethostname()
     print(hostname)
 
-    ExploreDrone("35.3.34.40", 8001, "35.3.34.40", 8000, "backup") # For now, just simulating on one laptop, so same hostname for both manager and explorer
+    ExploreDrone("localhost", 8001, "localhost", 8000, "backup") # For now, just simulating on one laptop, so same hostname for both manager and explorer
     # ExploreDrone("192.168.1.22", 8001, "192.168.1.22", 8000, coords_list)
 
 if __name__ == "__main__":
